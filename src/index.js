4819edbabcdf47d0dd62163ec0d1d746b9c1bec5
Tom Horton <tom@visiongift.org>
2022-05-17 11:56:20 -0700

    created buttons for each record

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import { companiesData } from "./companies-data";
 
 const extraRecord = {
@@ -8,17 +9,70 @@ const extraRecord = {
 };
 
 const before = document.getElementById("before");
-before.innerHTML = JSON.stringify(companiesData, null, 2);
+const after = document.getElementById("after");
+
 
 //TODO: Extract an element from the array.
-//TODO: Add elements to beginning and end of array.
-//TODO: Remove Elements from end of array.
-//TODO: Create a new array from old data.
-//TODO: Filter for a certain state.
-//TODO: Find the first element with a certain name.
-//TODO: Create a new array with just elements of a certain state.
-//TODO: Create buttons for each record.
-//functions are here
-const manipulatedData = [];
+// const manipulatedData = companiesData[0];
+// const manipulatedData = companiesData.length;
 
-after.innerHTML = JSON.stringify(manipulatedData, null, 2);
+//TODO: Add elements to beginning and end of array.
+// const manipulatedData = companiesData.pop();
+
+//TODO: Remove Elements from end of array.
+// const manipulatedData = companiesData.shift();
+// const manipulatedData = companiesData.unshift(extraRecord); //start
+// const manipulatedData = companiesData.push(extraRecord); //end
+
+//TODO: Create a new array from old data.
+
+// const manipulatedData = companiesData.map(mapOverData);
+
+//TODO: Filter for a certain state.
+// const filterForState = function (company) {
+//   return company.fieldData.State === "OR";
+// };
+// const manipulatedData = companiesData.filter(filterForState);
+
+//TODO: Find the first element with a certain name.
+// const manipulatedData = companiesData.find(filterForState);
+
+//TODO: Create a new array with just elements of a certain state.
+// const manipulatedData = companiesData.filter(filterForState).map(mapOverData);
+
+//TODO: Create buttons for each record.
+
+//functions are here
+
+const mapOverData = function (data) {
+  return {
+  company: data.fieldData.CompanyName, 
+  state: data.fieldData.State,
+};
+};
+
+const filterForState = function (company) {
+  return company.fieldData.State === "TX";
+};
+
+const sortBy = (a, b) => 
+  a.fieldData.CompanyName > b.fieldData.CompanyName ? 1 : -1;
+
+const doForEach = function (company, i) {
+  const button = document.createElement("button");
+  button.innerHTML = company.fieldData.CompanyName;
+  button.className = "btn btn-sm btn-warning m-1 col-5";
+  button.id = company.fieldData.Id;
+  button.addEventListener("click", function () {
+    // alert("Howdy");
+    console.log(this.id);
+  });
+  after.appendChild(button);
+
+};
+
+companiesData.sort(sortBy).forEach(doForEach);
+// companiesData.filter(filterForState).forEach(doForEach);
+
+// after.innerHTML = JSON.stringify(manipulatedData, null, 2);
+// before.innerHTML = JSON.stringify(companiesData, null, 2);
